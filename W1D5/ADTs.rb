d3cf218bc734e3d69a85b413c749163ce6fd3384
Jstuff36 <mizzou_fan10@hotmail.com>
2017-06-01 20:24:57 -0700

    My first queue

--- W1D5/ADTs.rb
+++ W1D5/ADTs.rb
@@ -19,3 +19,25 @@ class Stack
   end
 
 end
+
+class Queue
+
+  attr_reader :queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    queue.unshift(el)
+  end
+
+  def dequeue
+    queue.pop
+  end
+
+  def show
+    p queue
+  end
+
+end
