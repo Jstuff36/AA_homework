5a5b8b7d677cf214c2e29af268dc1f174b7cdc49
Jstuff36 <mizzou_fan10@hotmail.com>
2017-06-01 20:38:46 -0700

    A simple map algorithm

--- W1D5/ADTs.rb
+++ W1D5/ADTs.rb
@@ -41,3 +41,37 @@ class Queue
   end
 
 end
+
+class My_Map
+
+  attr_reader :my_map
+
+  def initialize
+    @my_map = []
+  end
+
+  def assign(key, value)
+    if lookup(key)
+      my_map.each_with_index do |pair, i|
+        my_map[i][1] = value if key == pair[0]
+      end
+    else
+      my_map << [key, value]
+    end
+
+  end
+
+  def lookup(key)
+    my_map.each do |pair|
+      return pair[1] if pair[0] == key
+    end
+    false
+  end
+
+  def remove(key)
+    my_map.each_with_index do |pair, i|
+      my_map.delete_at(i) if pair[0] == key
+    end
+  end
+
+end
