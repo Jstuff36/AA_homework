8bb1202ec086d6416b73433541754fb9ba2c7f8d
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-04 21:08:35 -0700

    Created a small js script to practice AJAX

--- W6D2/skeleton/js/index.js
+++ W6D2/skeleton/js/index.js
@@ -1,5 +1,12 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+$.ajax( {
+  method: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success(data) {
+    console.log(data);
+  }
+});
 // Add another console log here, outside your AJAX request
+console.log("Wow! This prints before the second console log");
