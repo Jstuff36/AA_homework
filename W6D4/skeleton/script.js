ed64b518b00c03ed75d051d3403705a4fe901190
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-05 20:09:55 -0700

    Created favorites list

--- W6D4/skeleton/script.js
+++ W6D4/skeleton/script.js
@@ -17,8 +17,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   // adding SF places as list items
+  const handleFavoriteSubmit = (e) => {
+    e.preventDefault();
 
-  // --- your code here!
+    const favoriteRestInput = document.querySelector(".favorite-input");
+    const favoriteRest = favoriteRestInput.value;
+    favoriteRestInput.value = "";
+
+    const newListLi = document.createElement("li");
+    newListLi.textContent = favoriteRest;
+
+    const favoritesList = document.getElementById("sf-places");
+    favoritesList.appendChild(newListLi);
+  };
+
+  const favListSubmitButton = document.querySelector(".favorite-submit");
+  favListSubmitButton.addEventListener("click", handleFavoriteSubmit);
 
 
 
